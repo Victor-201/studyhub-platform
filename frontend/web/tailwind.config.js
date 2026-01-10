aa978e7ac874bea784e665cb2409d1147a4e14c3
Victor-201 <4.victor.201@gmail.com>
2026-01-11 04:21:13 +0700

    style(frontend): update global styles and tailwind configuration

--- frontend/web/tailwind.config.js
+++ frontend/web/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import lineClamp from "@tailwindcss/line-clamp";
+
 export default {
-  darkMode: "class", 
+  darkMode: "class",
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
-  plugins: [],
-}
+  plugins: [lineClamp],
+};
